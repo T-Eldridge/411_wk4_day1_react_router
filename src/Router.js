5d519f6f42c1b3a7e07258f6080dc36dbcfb35d4
TEldridge <skippydrumskins@yahoo.com>
2020-10-03 22:55:53 -0500

    all objectives met

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,30 @@
+import Home from './components/Home';
+import About from './components/About'
+import React from 'react'
+import { Switch, Route } from 'react-router'
+import Car from './components/Car'
 /* 
     Write code for React Router here
 Import the Home and About components from 
 the component folder. Assign the default
 path "/" to the Home component and "/about"
 to the About component.
-
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Switch, Route } from 'react-router'
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/car/:id" component={Car} />
+        </Switch>
+    )
+}
+    
 // Write component imports here //
 
 
 
 // Start Router function here //
+export default Router;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import cars from '../cars.json'
+import {Container, Paper, Chip, Typography, makeStyles} from '@material-ui/core';
 // import material ui components here //
 // Container, Paper, Chip //
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    containerSm: {
+        padding: '40px',
+    },
+    carName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+}))
+
+
 const Car = (props) => {
+    const classes = useStyles()
+    const id = parseInt(props.match.params.id)
+    const foundcar = cars.find(car => id === car.id)
+    console.log(foundcar)
     return (
-        <h1>A specific car</h1>
+       <Container className={classes.containerLarge} maxWidth="sm">
+           <Paper className="car-container">
+               <Container maxWidth='xs' className={classes.containerSm}>
+                   <Typography className={classes.carName}>
+                       {foundcar.Name}
+                   </Typography>
+                   <br/>
+                   <Chip label={`ID: ${foundcar.id}`}/>
+                   <Chip label={`MPG: ${foundcar.Miles_per_Gallon}`}/>
+                   <Chip label={`Cylinders: ${foundcar.Cylinders}`}/>
+                   <Chip label={`Displacement: ${foundcar.Displacement}`}/>
+                   <Chip label={`Horsepower: ${foundcar.Horsepower}`}/>
+                   <Chip label={`Weight: ${foundcar.Weight_in_lbs}`}/>
+                   <Chip label={`Acceleration: ${foundcar.Acceleration}`}/>
+                   <Chip label={`Year: ${foundcar.Year}`}/>
+                   <Chip label={`Origin: ${foundcar.Origin}`}/>
+               </Container>
+
+           </Paper>
+        
+       </Container>
     )
 }
 
